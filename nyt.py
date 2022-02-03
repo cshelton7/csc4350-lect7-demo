669359ddced69807bbbc0b9f64879de58ad8750f
cshelton7 <cshelton7@student.gsu.edu>
2022-02-02 21:31:12 -0500

    fixed return statement

--- nyt.py
+++ nyt.py
@@ -9,25 +9,26 @@ BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 
 def get_article_data(keyword):
     """ Returns a list of headlines about a given topic """
-for page in range(2):
-    params = {
-        'q': keyword,
-        'api-key': os.getenv('NYT_KEY'),
-    }
+    for page in range(2):
+        params = {
+            'q': keyword,
+            'api-key': os.getenv('NYT_KEY'),
+        }
 
-    response = requests.get(BASE_URL, params=params)
-    data = response.json()
-    articles = data['response']['docs']
+        response = requests.get(BASE_URL, params=params)
+        data = response.json()
+        articles = data['response']['docs']
 
-    def get_headline(article):
-        return article['headline']['main']
+        def get_headline(article):
+            return article['headline']['main']
 
-    def get_snippet(article):
-        return article['snippet']
+        def get_snippet(article):
+            return article['snippet']
 
-    headlines = map(get_headline, articles)
-    snippets = map(get_snippet, articles)
-    return {  
-    'headlines': list(headlines),
-    'snippets': list(snippets),
-    }
+        headlines = map(get_headline, articles)
+        snippets = map(get_snippet, articles)
+
+        return {  
+            'headlines': list(headlines),
+            'snippets': list(snippets),
+        }
